070b5be8f2e62bed83848728b8df1169c2a1b913
Miles Crighton <miles.crighton@gmail.com>
2020-03-05 11:25:39 +0000

    Finished timestamp Micro-service

--- routes/timestamp.js
+++ routes/timestamp.js
@@ -3,9 +3,25 @@ const router = express.Router();
 
 router.get('/:date_string?', (req, res) => {
     //Handle timestamp Microservice
-    var dateString = req.params.date_string;
-    console.log(dateString)
-    res.send('Hey')
+    const dateString = req.params.date_string;
+    const utcRegex = /\d{4}-\d{2}-\d{2}/g
+    const unixRegex = /\d{13}/g
+
+    let dateObj
+    if (!dateString) {
+        dateObj = new Date()
+    } else if (unixRegex.test(dateString)) {
+        dateObj = new Date(parseInt(dateString))
+    } else if (utcRegex.test(dateString)) {
+        dateObj = new Date(dateString)
+    }
+
+    console.log(`Calculated date: ${dateObj}`)
+    if (dateObj && dateObj !== "Invalid Date") {
+        res.send({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
+    } else {
+        res.send({ error: "Invalid Date" });
+    }
 })
 
 module.exports = router;
